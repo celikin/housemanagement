425777261d477ef8d13d954523df2d369430ed65
daniilr <i@daniil-r.ru>
2014-10-18 04:15:43 +1100

    Форма логин и пароль

--- tsj/forms.py
+++ tsj/forms.py
@@ -1,8 +1,12 @@
+# -*- coding: utf-8 -*-
+from django import forms
 from django.forms import ModelForm
 from .models import Company, Resident
 
 
 class CompanyForm(ModelForm):
+    username = forms.CharField(label=u"Имя пользователя")
+    password = forms.CharField(label=u"Пароль", widget=forms.PasswordInput())
 
     class Meta:
         model = Company
