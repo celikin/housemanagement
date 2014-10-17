1d8c0e91451e9b8945a96b684bc02fa4aafd715c
foobarrior <nikitamalyavin@gmail.com>
2014-10-17 22:43:02 +1100

    fix typo

--- housem/urls.py
+++ housem/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'tsj.views.home', name='home'),
-    url(r'^/registration/', 'tsj.views.registration')
-    url(r'^/register/', 'tsj.views.register')
+    url(r'^/registration/', 'tsj.views.registration'),
+    url(r'^/register/', 'tsj.views.register'),
     url(r'^admin/', include(admin.site.urls)),
 )
