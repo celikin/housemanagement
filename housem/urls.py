427de8ed70e5973fc7ef860533a1ff31f5a8f5a8
foobarrior <nikitamalyavin@gmail.com>
2014-10-17 22:36:19 +1100

    Add registration carcase

--- housem/urls.py
+++ housem/urls.py
@@ -4,5 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'tsj.views.home', name='home'),
+    url(r'^/registration/', 'tsj.views.registration')
+    url(r'^/register/', 'tsj.views.register')
     url(r'^admin/', include(admin.site.urls)),
 )
--- tsj/views.py
+++ tsj/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
 
 def home(request):
     return render(request, "home.html")
+
+def registration(request):
+	if request.user.is_authenticated():
+		return redirect(reverse('home'))
+	else:
+		return render(request, "registration.html")
+
+def register(request):
+	return redirect(reverse('home'))
