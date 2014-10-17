0cf3d74095f039992c7d854c5f1c6d06e92a5d3f
daniilr <i@daniil-r.ru>
2014-10-17 21:38:09 +1100

    Исправления по модели

--- tsj/models.py
+++ tsj/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-from django.contrib.auth import User
+from django.contrib.auth.models import User
 
 
 class Street(models.Model):
@@ -32,8 +32,8 @@ class Company(models.Model):
     bill_numb = models.CharField(max_length=15, verbose_name=u"Расчетный счет")
     bank_name = models.CharField(max_length=150, verbose_name=u"В ОАО Банк")
     kor_schet = models.CharField(max_length=15, verbose_name=u"Кор. счет")
-    bik = models.CharField(max_length=15, verbose_name=u"Кор. счет")
-    workgraph = models.TextField(verbose_name=u"Кор. счет")
+    bik = models.CharField(max_length=15, verbose_name=u"БИК")
+    workgraph = models.TextField(verbose_name=u"График работы")
 
 
 class Resident(models.Model):
