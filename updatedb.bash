add2195e5291bfa8b73caf1f6e98e9a9e76c7881
foobarrior <nikitamalyavin@gmail.com>
2014-10-19 17:43:33 +1100

    инициалка для счётчиков

--- updatedb.bash
+++ updatedb.bash
@@ -2,4 +2,4 @@
 set -x
 ./manage.py makemigrations &&
 	./manage.py migrate &&
-	./manage.py loaddata initial companies srvprov arbitrary
+	./manage.py loaddata initial companies srvprov arbitrary meter_types
