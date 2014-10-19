e68c6d3fe9833d12e767b5560dda0839ba3dbbc3
daniilr <i@daniil-r.ru>
2014-10-19 17:53:09 +1100

    Продакшн

--- tsj/wsgi.py
+++ tsj/wsgi.py
@@ -1,10 +1,10 @@
 """
-WSGI config for housem project.
+WSGI config for blog3 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import sys
@@ -14,3 +14,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "housem.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+#########################################
